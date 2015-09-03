225047fec7dfe13f95413fb8996af37d75dfef2e
Joe Lambert <joe@joelambert.co.uk>
2015-09-03 14:27:39 +0100

    Initial commit

---
+++ src/Handlebars/HandlebarsTemplate.php
@@ -0,0 +1,44 @@
+<?php namespace Rareloop\Primer\TemplateEngine\Handlebars;
+
+use Rareloop\Primer\Templating\Template as PrimerTemplate;
+use Rareloop\Primer\TemplateEngine\Handlebars\Handlebars;
+// use \Handlebars\Handlebars;
+use Rareloop\Primer\Primer;
+
+
+
+class Template extends PrimerTemplate
+{
+    /**
+     * Array of file extensions
+     *
+     * @var array
+     */
+    protected $extension = 'hbs';
+
+    public function load($directory, $filename)
+    {
+        parent::load($directory, $filename);
+
+        $template = false;
+
+        $path = $directory . '/' . $filename . '.' . $this->extension;
+
+        if(is_file($path)) {
+            $template = file_get_contents($path);
+        }
+
+        if(!$template) {
+            throw new \Exception('Template can not be found: ' . $directory . '/' . $filename);
+        }
+
+        $this->template = $template;
+    }
+
+    public function render($data)
+    {
+        $engine = Handlebars::instance();
+
+        return $engine->render($this->template, $data);
+    }
+}
